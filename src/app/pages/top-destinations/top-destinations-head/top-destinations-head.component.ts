ef01a95c3a74c8fdeceba235791daf40bb445310
Rajkamal-das <rajkamal.das@webviotechnologies.com>
2025-08-03 14:59:56 +0530

    top destination table

--- src/app/pages/top-destinations/top-destinations-head/top-destinations-head.component.ts
+++ src/app/pages/top-destinations/top-destinations-head/top-destinations-head.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-destinations-head',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './top-destinations-head.component.html',
   styleUrl: './top-destinations-head.component.scss'
 })
-export class TopDestinationsHeadComponent {
+export class TopDestinationsHeadComponent implements OnInit {
+  activeroute: string = '';
+  private router: Router = inject(Router);
+  private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    this.setActiveTabBasedOnRoute(this.router.url);
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveTabBasedOnRoute(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.setActiveTabBasedOnRoute(this.router.url);
+  }
+
+setActiveTabBasedOnRoute(url: string): void {
+  const segments = url.split('/').filter(Boolean); 
+  const lastSegment = segments[segments.length - 1]; 
+  console.log('Last segment:', lastSegment);
+  this.activeroute = lastSegment;
+  this.cdr.detectChanges();
+}
 
 }
--- src/app/pages/top-destinations/top-destinations-list/top-destinations-list.component.ts
+++ src/app/pages/top-destinations/top-destinations-list/top-destinations-list.component.ts
@@ -1,11 +1,40 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-destinations-list',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './top-destinations-list.component.html',
   styleUrl: './top-destinations-list.component.scss'
 })
 export class TopDestinationsListComponent {
+ destinations: string[] = [
+    'Istanbul', 'London', 'Madrid', 'Maldives', 'New York', 'Paris',
+    'Rome', 'Singapore', 'Switzerland', 'San Francisco', 'Los Angeles', 'Lake Tahoe',
+    'Santa Monica', 'Oregon', 'Seattle', 'St.george Utah', 'Death Valley National Park', 'Glacier National Park',
+    'Yosemite National Park', 'Martha Vineyard', 'Miami', 'Nashville, Tennessee', 'Philadelphia, Pennsylvania', 'Tampa',
+    'Charleston, South Carolina', 'Lake Placid, Ny', 'Acadia National Park', 'Great Smoky Mountains National Park', 'Savannah', 'Sedona, Arizona',
+    'Portland, Maine', 'Montpelier, Vermont', 'Finger Lakes', 'Massachusetts', 'Atlantic City', 'Manchester, New Hampshire',
+    'Washington Dc', 'Greenville, Sc', 'Montgomery', 'Athens', 'Victoria Island', 'Inuvik',
+    'Yellow Knife', 'Fort Smith', 'Tuktoyaktuk', 'Vancouver', 'Tofino', 'Calgary',
+    'Whistler', 'Jasper National Park', 'Churchill', 'Nova Scotia', 'Prince Edward Island', 'Lunenburg',
+    'Moncton', 'Hamilton', 'Toronto', 'Ottawa', 'Montreal', 'London, Ontario',
+    'Kitchener', 'Quebec City', 'Saskatoon', 'Drumheller', 'Regina', 'Breckenridge, Colorado',
+    'Girdwood, Alaska', 'Jackson Hole, Wyoming', 'Park City, Utah', 'Cancun, Mexico', 'Paris, Europe', 'Punta Cana',
+    'Alabama', 'California, Usa', 'Texas, United States', 'Banff, Alberta', 'Fernie, British Columbia', 'Okanagan Valley, Columbia',
+    'The Laurentians, Quebec', 'Abu Dhabi', 'India', 'Hong Kong, China', 'Sydney, Australia', 'Niagara Falls, Canada',
+    'Italy'
+  ];
 
+  destinationRows: string[][] = [];
+
+  constructor() {
+    this.chunkDestinations(7); // Split into rows of 7
+  }
+
+  chunkDestinations(size: number): void {
+    for (let i = 0; i < this.destinations.length; i += size) {
+      this.destinationRows.push(this.destinations.slice(i, i + size));
+    }
+  }
 }
