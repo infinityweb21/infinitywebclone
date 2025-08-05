7f8c30a06215caa73857176df7bc7bfe5828b9d2
Subhrangsu <subhrangsubera@webviotechnologies.com>
2025-08-05 17:23:48 +0530

    Fix flight booking payment form: update input types to number, ensure string conversion for card details, and improve booking status handling

--- src/app/pages/flights-list/flight-details/select-seats/flight-booking-payment/flight-booking-payment.component.ts
+++ src/app/pages/flights-list/flight-details/select-seats/flight-booking-payment/flight-booking-payment.component.ts
@@ -526,8 +526,8 @@ const paymentPayload = {
   Email: contactInfo.email || '',
   PaymentType: 'CC',
   CardType: this.getCardBrand(formValue.CardNumber),
-  CardNumber: formValue.CardNumber,
-  CVV: formValue.CVV,
+  CardNumber: formValue.CardNumber.toString(),
+  CVV: formValue.CVV.toString(),
   ExpiryDate: formValue.ExpiryDate,
   BillingPhoneNum: formValue.BillingPhoneNum.number,
   Name: contactInfo.firstName || '', // You can also combine first/last if needed
@@ -548,8 +548,8 @@ const confirmPayload = {
   Email: contactInfo.email || '',
   PaymentType: 'CC',
   CardType: this.getCardBrand(formValue.CardNumber), // fallback to 'CA' if needed
-  CardNumber: formValue.CardNumber,
-  CVV: formValue.CVV,
+  CardNumber: formValue.CardNumber.toString(),
+  CVV: formValue.CVV.toString(),
   ExpiryDate: formValue.ExpiryDate,
   BillingPhoneNum: formValue.BillingPhoneNum.number,
   Name: contactInfo.firstName || '', // You can also combine first/last if needed
@@ -569,11 +569,13 @@ if(this.flattenedSeatData.length!=0){
           this.flightBookingResponse = res?.data;
 
       // Show toast
-      this.toasterService.showSuccess(
-        res?.data?.bookingStatus === 'Success'
-          ? 'Booking flight successful'
-          : 'Booking failed'
-      );
+      if (res?.data?.bookingStatus === 'Success') {
+        this.toasterService.showSuccess('Booking flight successful');
+      }
+
+      if (res?.data?.bookingStatus === 'Failed') {
+        this.toasterService.showError(res.data?.errorsList.tperror[0]?.errorText);
+      }
 
       // Continue booking creation if successful
       if (res?.data?.bookingStatus === 'Success') {
@@ -987,10 +989,10 @@ const mainPayload={
     "airline3pnr": "",
     "card_type": this.getCardBrand(formValue.CardNumber),
     "name_oncard": formValue.cardHolder,
-    "cardno":  formValue.CardNumber,
+    "cardno":  formValue.CardNumber.toString(),
     "exp_month": exp_month,
     "exp_year":exp_year,
-    "cvv":formValue.CVV,
+    "cvv":formValue.CVV.toString(),
     "billing_address":formValue.street,
     "street_number": "",
     "locality": "",
